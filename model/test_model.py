58c1414a9fafd0a95f90c8f91b88407cd97ca50f
Piotr Walesiak <piotr.walesiak@xclinical.com>
2023-10-01 09:25:13 +0200

    Test model upgraded

--- model/test_model.py
+++ model/test_model.py
@@ -1,45 +1,73 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import math
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import sqlite3
+
+def main(user_model_conversation_history: list[str], path_to_database: str):
+    path_to_database = path_to_database
+    default_model_task = "-- Task: Using valid SQLite, answer the following questions for the tables provided above.\n\n"
 
 
-def main(api_request, target_db):
-    x_train = torch.linspace(0, 2 * math.pi, 1000).view(-1, 1)  # Reshape to (1000, 1)
-    y_train = torch.sin(x_train)
+    def create_database_schema():
+        conn = sqlite3.connect(path_to_database)
 
-    class SimpleNN(nn.Module):
-        def __init__(self):
-            super(SimpleNN, self).__init__()
-            self.fc1 = nn.Linear(1, 10)  # Input Layer to Hidden Layer
-            self.fc2 = nn.Linear(10, 1)  # Hidden Layer to Output Layer
+        cur = conn.cursor()
 
-        def forward(self, x):
-            x = torch.relu(self.fc1(x))
-            x = self.fc2(x)
-            return x
+        # Execute a SQL query to retrieve all table names in the database
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
 
-    # Model, Loss function and Optimizer
-    model = SimpleNN()
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+        # Fetch all results of the query
+        tables = cur.fetchall()
 
-    # Training Loop
-    epochs = 20
-    for epoch in range(epochs):
-        model.train()
-        optimizer.zero_grad()
-        y_pred = model(x_train)
-        loss = criterion(y_pred, y_train)
-        loss.backward()
-        optimizer.step()
+        # Iterate through the results and print the table names along with their column names and types
+        database_schema = ""
+        for table in tables:
+            table_name = table[0]
 
-        if epoch % 10 == 0:
-            print(f"Epoch {epoch}, Loss: {loss.item()}")
+            # Execute a query to get the column information for the current table
+            cur.execute(f"PRAGMA table_info({table_name});")
 
-    # Test the model
-    model.eval()
-    x_test = torch.tensor([[math.pi/2]])  # sin(pi/2) = 1
-    y_test = model(x_test)
+            # Fetch all column information
+            columns = cur.fetchall()
 
-    return f"sin(pi/2) ≈ {y_test.item()}\nAPI request - {api_request} - {target_db}"
+            # Extract column names and types and format the string
+            column_info = ', '.join([f"{column[1]} {column[2]}" for column in columns])
+            formatted_string = f"CREATE TABLE {table_name}({column_info})\n\n"
+            database_schema += formatted_string
+
+
+        # Close the cursor and the connection
+        cur.close()
+        conn.close()
+
+        return database_schema
+
+
+    def xxx():
+        pre_prompt = ""
+        for i in range(len(user_model_conversation_history)):
+            if i%2 == 0:
+                pre_prompt += default_model_task
+            pre_prompt += user_model_conversation_history[i]
+            pre_prompt += "\n\n"
+
+        return pre_prompt
+    #database_schema = create_database_schema()
+
+    prompt=''
+    prompt += create_database_schema()
+    prompt += xxx()
+    prompt += 'SELECT'
+
+
+    def call_model(prompt):
+        model_name = "NumbersStation/nsql-350M"
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(model_name)
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+        generated_ids = model.generate(input_ids, max_length=1500)
+        output = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        output = 'SELECT' + output.split('SELECT')[-1]
+
+        return output
+
+    print(call_model(prompt))
+
